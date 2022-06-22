83e5c1e458a314e669b24b992250cac0844ff812
somin Lee <85738589+nimoseel@users.noreply.github.com>
2022-06-22 23:31:39 +0900

    Add files via upload

---
+++ Redux/007_use_reduxtoolkit/my-app/src/modules/goodsCounter.jsx
@@ -0,0 +1,68 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+// // 액션 생성함수 : 실수를 줄이고, 코드의 양을 줄이기 위해 사용
+// export const addNumber = () => {
+//     return {type : 'ADD'}
+// }
+
+// export const substractNumber = () => {
+//     return {type : 'SUBSTRACT'}
+// }
+
+// // 초기값
+// const initialState = {
+//     stock : 10, // 재고
+//     goods : 1    // user의 파우치 구매 개수
+// }
+
+// const goodsReducer = (state = initialState, action) => {
+//     console.log(action)
+//     console.log(action.type)
+//     console.log(state)
+//     switch (action.type) {
+//         case 'ADD':
+//             return {
+//                 ...state,
+//                 stock : state.stock - 1,
+//                 goods : state.goods + 1,
+//             }
+//         case 'SUBSTRACT':
+//             return {
+//                 ...state,
+//                 stock : state.stock + 1,
+//                 goods : state.goods - 1,
+//             }
+//         default:
+//             return state
+//     }
+// }
+
+// export default goodsReducer
+
+// 초기값
+const initialState = {
+    stock : 10, // 재고
+    goods : 1    // user의 파우치 구매 개수
+}
+
+export const counterSlice = createSlice ({
+    name: 'counter', // 리듀서의 네임
+    initialState,
+    reducers: {
+        //오브젝트 형태로 작성
+        increment: (state) => {
+            state.stock -= 1
+            state.goods +=1
+        },
+        decrement: (state) => {
+            state.stock += 1
+            state.goods -= 1
+        }
+        //함수이름:함수, 
+    }
+})
+
+console.log(counterSlice)
+
+export const {increment, decrement} = counterSlice.actions
+export default counterSlice.reducer
