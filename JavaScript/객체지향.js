6944cf3ea38d31c294a905ae2343eb180adcec6b
somin Lee <85738589+nimoseel@users.noreply.github.com>
2022-07-12 00:03:54 +0900

    Update 객체지향.js

--- JavaScript/객체지향.js
+++ JavaScript/객체지향.js
@@ -1,13 +1,12 @@
-객체를 지향한다?
-객체를 많이 만들겠다
+// 객체를 지향한다? = 객체를 많이 만들겠다
 
-객체 : 행동, 상태를 가짐 
-행동은 메소드, 상태는 프로퍼티 
+// 객체 : 행동, 상태를 가짐 
+// 행동은 메소드, 상태는 프로퍼티 
 
 const me = {
-    name : '이소민 ',
-    address : '경기도 시흥시 OO길',
-    phoneNum : '010-8515-2311',
+    name : '000',
+    address : '경기도 00시 OO길',
+    phoneNum : '000-0000-0000',
     canWalk : function(){
         console.log('재현이가 걷는다.');
     },
@@ -28,7 +27,6 @@ const student = {
 효율적이지 않음... 객체를 만들어낼 수 함수가 없을까? - 생성자 ! 
 생성자를 통해 생성된 객체는 같은 프로퍼티와 메서드를 공유할 수 있음 
 
-
 생성자는 기본적으로 함수 
 암묵적으로 대문자로 시작하는 이름을 가지는 것으로 약속되어 있음. 
 */
@@ -43,18 +41,17 @@ function NewFactory(name){
 }
 
 const robot1 = new NewFactory('재현');
-생성자 함수의 this는 인스턴스를 참조한다. 
+// 생성자 함수의 this는 인스턴스를 참조한다. 
 
 
-___
 function Food(name){
     this.name = name
     this.sya
 }
 
-this 문맥에 따라 가리키는 것이 달라짐 
-앞으로 만들어질 인스턴스 자기 자신을 가르키도록 사용할 수 있음  - this 바인딩
-/ 런타임 상에서 this바인딩 동적으로 결정 
+// this 문맥에 따라 가리키는 것이 달라짐 
+// 앞으로 만들어질 인스턴스 자기 자신을 가르키도록 사용할 수 있음  - this 바인딩
+// / 런타임 상에서 this바인딩 동적으로 결정 
 
 const foods = ['짜장면','볶음밥','치킨','해장국'];
 function FoodBot(foodNames){
@@ -63,17 +60,6 @@ function FoodBot(foodNames){
 
 const foodBotMark1 = new FoodBot(foods);
 
-소수점 나가리하려고 소수점...
-Math.random하면 0-1미만
-
-재현님 지금 코드에 foods.length하면 인자에 foods 말고 다른 배열 들어왔을 때 오류가 날 것 같습니다
-
-
-생성자가 클래스고 클래스가 타입이다
-
-자바스크립트는 타입 기반 언어 흑흑
-
-재현님 코드 
 
 // const me = {
         //     name: '한재현',
@@ -93,6 +79,7 @@ Math.random하면 0-1미만
         //         this.level++;
         //     }
         // }
+
 
         function Me(name, address, phoneNum) {
             this.name = name;
@@ -116,5 +103,5 @@ Math.random하면 0-1미만
             this.level++;
         }
 
-call - 프로퍼티 받아오기
-object.create - 함수 상속하기
+// call - 프로퍼티 받아오기
+// object.create - 함수 상속하기
